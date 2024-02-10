25c0a95bbad62ed488f64e62688bd39270846905
rileyhales <rileyhales1@gmail.com>
2024-02-09 20:04:42 -0700

    correct preparing environment

--- scripts/prepare_environment.sh
+++ scripts/prepare_environment.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Install the conda environment if it doesn't exist, otherwise update it to match the file
-mamba env create -f environment.yml
+mamba env create -f ../environment.yaml
