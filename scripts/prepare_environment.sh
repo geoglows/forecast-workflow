91a6f7fc52b2ce2893bf694f86078901d0d09ce9
rileyhales <rileyhales1@gmail.com>
2024-02-10 09:44:49 -0700

    auto activate new conda environment

--- scripts/prepare_environment.sh
+++ scripts/prepare_environment.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 # Install the conda environment if it doesn't exist, otherwise update it to match the file
-mamba env create -f ../environment.yaml
+mamba env create -f ../environment.yaml --name $CONDA_ENV || mamba env update -f ../environment.yaml --name $CONDA_ENV
+
+# set the default environment to the one we just created
+# if conda activate $CONDA_ENV is not already in .bashrc, add it
+grep -q "conda activate $CONDA_ENV" ~/.bashrc || echo "conda activate $CONDA_ENV" >> ~/.bashrc
