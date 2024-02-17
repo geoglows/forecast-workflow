46a150f0430d78d3c090e2d0c152043c887e4a7d
rileyhales <rileyhales1@gmail.com>
2024-02-16 20:09:16 -0700

    remove max processors count

--- bash/workflow.sh
+++ bash/workflow.sh
@@ -25,9 +25,6 @@ export YMD
 VPUS=$(ls -1 $CONFIGS_DIR | awk -F/ '{print $NF}' | sort -V)
 echo "Found ${#VPUS[@]} VPUs"
 
-NCPUS=$(($(nproc --all) < ${#VPUS[@]} ? $(nproc --all) : ${#VPUS[@]}))
-echo "Using $NCPUS CPUs"
-
 # Download the latest IFS grids from s3 bucket
   # compare the last downloaded date, and the date available on the s3 bucket
   # if the date available on the s3 bucket is newer than the last downloaded date, download the new data
@@ -42,28 +39,28 @@ mkdir -p $FORECASTS_DIR/$YMD/maptables
 
 # Calculate inflows
 echo "Calculating inflows"
-xargs -I {} -P $NCPUS python $HOME/forecast-workflow/python/prepare_inflows.py --ymd $YMD --vpu {} <<< $VPUS || exit 1
+xargs -I {} python $HOME/forecast-workflow/python/prepare_inflows.py --ymd $YMD --vpu {} <<< $VPUS || exit 1
 
 # Prepare namelists
 echo "Preparing namelists"
-xargs -I {} -P $NCPUS python $HOME/forecast-workflow/python/prepare_namelists.py --ymd $YMD --vpu {} <<< $VPUS || exit 1
+xargs -I {} python $HOME/forecast-workflow/python/prepare_namelists.py --ymd $YMD --vpu {} <<< $VPUS || exit 1
 
 # RAPID routing
 echo "Running RAPID routing"
 NAMELISTS=$(ls -1 $FORECASTS_DIR/$YMD/namelists/* | sort -V)
-xargs -I {} -P $NCPUS docker exec rapid python3 /mnt/scripts/runrapid.py --fcdir $FORECASTS_DIR/$YMD --namelist {} <<< $NAMELISTS || exit 1
+xargs -I {} docker exec rapid python3 /mnt/scripts/runrapid.py --fcdir $FORECASTS_DIR/$YMD --namelist {} <<< $NAMELISTS || exit 1
 
 # Concatenate and summarize the ensemble outputs
 echo "Concatenating and summarizing the ensemble outputs"
-xargs -I {} -P $NCPUS ./postprocess_rapid_outputs.sh --outputs $FORECASTS_DIR/$YMD/outputs --vpu {} <<< $VPUS || exit 1
+xargs -I {} ./postprocess_rapid_outputs.sh --outputs $FORECASTS_DIR/$YMD/outputs --vpu {} <<< $VPUS || exit 1
 
 # Calculate the init files
 echo "Calculating the init files"
-xargs -I {} -P $NCPUS python $HOME/forecast-workflow/python/calculate_inits.py --ymd $YMD --vpu {} <<< $VPUS || exit 1
+xargs -I {} python $HOME/forecast-workflow/python/calculate_inits.py --ymd $YMD --vpu {} <<< $VPUS || exit 1
 
 # Generate Esri map style tables
 echo "Generating Esri map style tables"
-xargs -I {} -P $NCPUS python $HOME/forecast-workflow/python/generate_vpu_map_tables.py --ymd $YMD --vpu {} <<< $VPUS || exit 1
+xargs -I {} python $HOME/forecast-workflow/python/generate_vpu_map_tables.py --ymd $YMD --vpu {} <<< $VPUS || exit 1
 python $HOME/forecast-workflow/python/generate_global_map_tables.py --ymd $YMD || exit 1
 
 # NetCDF to Zarr (and delete netCDFs)
